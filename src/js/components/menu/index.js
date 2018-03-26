4d8b460818f41582ccac4fbb68b063a0bf483b8f
Alexander Vielimchanitsia <alexandervielimchanitsia@gmail.com>
2018-03-26 09:16:39 +0300

    removed menu from window object

--- src/js/components/menu/index.js
+++ src/js/components/menu/index.js
@@ -8,6 +8,5 @@ import {Menu} from './models';
 export function createMenu(parentHeight: number, parentWidth: number, workingSpace: Object): MenuCtrl {
   const menu = new Menu(parentHeight, parentWidth, workingSpace);
   const view = new MenuView();
-  window.menu = new MenuCtrl(menu, view);
-  return window.menu;
+  return new MenuCtrl(menu, view);
 }
