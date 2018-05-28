9871005ec931cd92520148fd57c701c29c0d0bc3
Alexander Vielimchanitsia <alexandervielimchanitsia@gmail.com>
2018-05-28 22:59:51 +0300

    add drafts for modes

---
+++ src/js/core/modes/index.js
@@ -0,0 +1,9 @@
+
+// TODO: add state machine pattern for modes
+
+// Modes(states):
+//  - Edit: when we can change our electronic circuit
+//  - Run: when we're checking what the electronic circuit does
+
+// Context:
+// - setMode
